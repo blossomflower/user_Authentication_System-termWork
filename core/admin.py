fdab93add9fcd65e67da3977258bf5980071fca7
Anusha Rawat <99706326+blossomflower@users.noreply.github.com>
2023-01-26 16:31:24 +0530

    Add files via upload

---
+++ core/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+
+from .forms import UserCreationForm, UserEditForm
+from .models import User
+
+
+class UserAdmin(auth_admin.UserAdmin):
+    add_form = UserCreationForm
+    form = UserEditForm
+    model = User
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
